5715994bdf67394775d65d98fe3b1f6dec5ad7e2
Louise Nyambati <louisen1993@gmail.com>
2017-04-28 16:52:58 -0500

    added css file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,33 @@
 // Data
 var friendData = require("../data/friendData");
+
+module.exports = function(app) {
+
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
+
+  app.post("/api/tables", function(req, res) {
+
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  app.post("/api/clear", function() {
+
+    tableData = [];
+    waitListData = [];
+
+    console.log(tableData);
+  });
+};
