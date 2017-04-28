8428a335978ecab50173b842cbad9130b926c31d
Louise Nyambati <louisen1993@gmail.com>
2017-04-28 16:04:50 -0500

    added express

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+// Dependencies
 var express = require("express");
 var path = require("path");
 var body-parser = require("body-parser");
+
+// Sets up Express
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
