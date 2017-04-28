ecdb5c2c79191d0de4f5f659fa92f351a9ec34d6
Louise Nyambati <louisen1993@gmail.com>
2017-04-28 16:07:30 -0500

    finished server.js

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
